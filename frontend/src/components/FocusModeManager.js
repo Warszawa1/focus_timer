14a3477104e9827e4159ed36e9d4b616ef1568fb
Irene Antelo <iantelovazquez@gmail.com>
2024-12-11 17:13:03 +0100

    Fixed focusmode build errors

---
+++ frontend/src/components/FocusModeManager.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+function FocusModeManager({ isActive }) {
+    useEffect(() => {
+      if (isActive) {
+        // Request notification permissions
+        if ('Notification' in window) {
+          Notification.requestPermission();
+        }
+  
+        // Register service worker for notification blocking
+        if ('serviceWorker' in navigator) {
+          navigator.serviceWorker.register('/service-worker.js');
+        }
+      }
+    }, [isActive]);
+  
+    return null;
+  }
+  
+  export default FocusModeManager;
